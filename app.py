92207025f869e72eaafaabe2867d1993e2274b9c
ponterik <pontus.z.eriksson@outlook.com>
2021-03-16 10:24:10 +0100

    disconnects

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 game = Game()
-connected_players = 0
+connected_players = {}
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,28 +17,31 @@ def handle_input(data):
     global game
     input_data = data['data']
     game.update_input(input_data['player_id'], input_data['keypressed'])
-
+    
 @socketio.on('client_connected')
 def handle_client_connect_event(data):
     print("connected")
-    #Timer(1, game_loop, args = [1]).start()
     global connected_players
     global game
-
-    connected_players = connected_players + 1
-
-    player_id = connected_players
-    emit('assign_id', player_id)
-    game.add_player(player_id)
-
-    print(connected_players)
-    if connected_players == game.player_limit:
+    #should prob use request.sid
+    if not 1 in connected_players.values():
+        connected_players[request.sid] = 1
+        emit('assign_id', 1)
+        game.add_player(1)
+    elif not 2 in connected_players.values():
+        connected_players[request.sid] = 2
+        emit('assign_id', 2)
+        game.add_player(2)
+    
+    if len(connected_players) == game.player_limit:
         game.initialize_game()
         Timer(1, game_loop).start()
 
 
 @socketio.on('disconnect')
 def disconnected():
+    global connected_players
+    connected_players.pop(request.sid)
     print('disconnected')
 
 def game_loop():
